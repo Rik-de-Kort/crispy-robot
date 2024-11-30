eb17d18316f71aa5c8b80a340edf766b2bdb733a
Rik <rik.de.kort@hey.com>
2024-11-30 13:46:40 +0100

    Send crane state from back to frontend

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,14 +1,44 @@
 // Code based on https://github.com/snapview/tokio-tungstenite/blob/master/examples/echo-server.rs
+use std::sync::{Arc, Mutex};
 use std::{env, io::Error};
-
-use futures_util::{future, StreamExt, TryStreamExt};
+use std::fmt::Debug;
+use std::time::Duration;
+use futures_util::{StreamExt, SinkExt};
 use log::info;
+use serde::{Deserialize, Serialize};
 use tokio::net::{TcpListener, TcpStream};
+use tokio_tungstenite::tungstenite::Result;
+
+#[derive(Serialize, Deserialize, Debug)]
+struct Millimeters(u64); // Max is 1.95 light years, should fit
+#[derive(Serialize, Deserialize, Debug)]
+struct Degrees(u16); // Modulo 360
+
+// CraneState fits in 2*64 + 2*16 = 160 bytes
+#[derive(Serialize, Deserialize, Debug)]
+struct CraneState {
+    swing_rotation: Degrees,
+    elbow_rotation: Degrees,
+    lift_elevation: Millimeters,
+    gripper_open: Millimeters,
+}
+
+
+type Crane = Arc<Mutex<CraneState>>;
 
 #[tokio::main]
 async fn main() -> Result<(), Error> {
     let _ = env_logger::try_init();
-    let addr = env::args().nth(1).unwrap_or_else(|| "127.0.0.1:8080".to_string());
+    let addr = env::args()
+        .nth(1)
+        .unwrap_or_else(|| "127.0.0.1:8080".to_string());
+
+    let crane = Arc::new(Mutex::new(CraneState {
+        swing_rotation: Degrees(0),
+        lift_elevation: Millimeters(0),
+        elbow_rotation: Degrees(0),
+        gripper_open: Millimeters(0),
+    }));
 
     // Create the event loop and TCP listener we'll accept connections on.
     let try_socket = TcpListener::bind(&addr).await;
@@ -16,14 +46,16 @@ async fn main() -> Result<(), Error> {
     info!("Listening on: {}", addr);
 
     while let Ok((stream, _)) = listener.accept().await {
-        tokio::spawn(accept_connection(stream));
+        tokio::spawn(accept_connection(stream, crane.clone()));
     }
 
     Ok(())
 }
 
-async fn accept_connection(stream: TcpStream) {
-    let addr = stream.peer_addr().expect("connected streams should have a peer address");
+async fn accept_connection(stream: TcpStream, crane: Crane) -> Result<()> {
+    let addr = stream
+        .peer_addr()
+        .expect("connected streams should have a peer address");
     info!("Peer address: {}", addr);
 
     let ws_stream = tokio_tungstenite::accept_async(stream)
@@ -32,10 +64,32 @@ async fn accept_connection(stream: TcpStream) {
 
     info!("New WebSocket connection: {}", addr);
 
-    let (write, read) = ws_stream.split();
-    // We should not forward messages other than text or binary.
-    read.try_filter(|msg| future::ready(msg.is_text() || msg.is_binary()))
-        .forward(write)
-        .await
-        .expect("Failed to forward messages")
+    let (mut write, mut read) = ws_stream.split();
+    let mut interval = tokio::time::interval(Duration::from_millis(1000));
+    loop {
+        tokio::select! {
+            msg = read.next() => {
+                match msg {
+                    Some(msg) => {
+                        let msg = msg?;
+                        if msg.is_text() || msg.is_binary() {
+                            write.send(msg.into()).await?;
+                        } else if msg.is_close() {
+                            break;
+                        }
+                    },
+                    None => break,
+                }
+            }
+            _ = interval.tick() => {
+                let msg = {
+                    // Todo: error handling here
+                    let inner_state = &*crane.lock().unwrap();
+                    serde_json::to_string(inner_state).unwrap()
+                };
+                write.send(msg.into()).await?;
+            }
+        }
+    }
+    Ok(())
 }
